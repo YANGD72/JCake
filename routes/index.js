9d1330a28f383771e4a7d1feda54247070d55a86
YANGD72 <21100397@handong.edu>
2016-09-07 14:30:20 +0900

    MySQL Install

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,9 @@ router.get('/main', function(req, res, next){
   res.render('main')
 })
 
+
+router.get('/introduce', function(req, res, next){
+  res.render('introduce')
+})
+
 module.exports = router;
